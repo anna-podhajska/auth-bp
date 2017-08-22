4ec0ecedf5a97833d51d7c92f7ab7ee071eb12c8
todddrinkwater <daniel-reason>
2017-08-22 17:08:07 +1200

    work in progress

--- client/components/LoginForm.jsx
+++ client/components/LoginForm.jsx
@@ -1,22 +1,37 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
+//ania - skoncz to tak jak inne komnponenty:
+
 class LoginForm extends React.Component {
    constructor(props) {
      super(props)
      this.state = {
-
+       username: '',
+       password: ''
      }
    }
 
+handleSubmit(){
+  e.preventDefault()
+  // wyslij do servera!
+}
+
+handleOnChange(e) {
+
+  // this.set.state({
+  //   [e.target.name]: e.target.value
+  // })
+}
 
    render() {
      return (
        <div>
          <h1>Login!</h1>
-         <form>
-           <input type="text" name="username" placeholder="username" />
-           <input type="password" name="hash" placeholder="password" />
+         <form onSubmit={this.handleSubmit.bind(this)}>
+             <input type="text" name="username" placeholder="username" onChange={this.handleOnChange.bind(this)} />
+           <input type="password" name="password" placeholder="password"
+             onChange={this.handleOnChange.bind(this)} />
            <input type="submit" value="Login" />
          </form>
        </div>
@@ -24,4 +39,10 @@ class LoginForm extends React.Component {
    }
  }
 
- export default connect()(LoginForm)
+ function mapDispatchToProps(dispatch) {
+     return {
+       onSubmit: (creds) => {dispatch(handleSubmit(creds))}
+     }
+ }
+
+ export default connect(mapDispatchToProps)(LoginForm)
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -32,5 +32,6 @@ router.get('/username', token.decode, (req, res) => {
   })
 })
 
+// console.log(req.body);
 
 module.exports = router
